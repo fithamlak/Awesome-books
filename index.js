b07df2aa0374aa76cb8ca30852bb3498a07f6523
Fithamlak <justiceofgod01@gmail.com>
2023-01-16 22:15:57 +0300

    Display all books saved in the collection in the top part of the page.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-// Create a collection that keeps a list of books
+const dynamicCreation = document.querySelector('.daynamic');
 
+// Create a collection that keeps a list of books
 const listOfBooks = [
   {
     title: 'atomic habit',
@@ -28,3 +29,24 @@ function removeBook(index) {
   listOfBooks.splice(listOfBooks.findIndex((e) => e.title === listOfBooks[index].title
   && e.author === listOfBooks[index].author), 1);
 }
+// Display all books saved in the collection in the top part of the page.
+function bookLoders() {
+  for (let k = 0; k < listOfBooks.length; k += 1) {
+    const book = document.createElement('article');
+    const title = document.createElement('p');
+    title.innerText = listOfBooks[k].title;
+    const author = document.createElement('p');
+    author.innerText = listOfBooks[k].author;
+    book.append(title);
+    book.append(author);
+    const button = document.createElement('button');
+    button.innerText = 'Remove';
+    book.append(button);
+    const line = document.createElement('hr');
+    book.append(line);
+    dynamicCreation.append(book);
+  }
+}
+window.onload = () => {
+  bookLoders();
+};
