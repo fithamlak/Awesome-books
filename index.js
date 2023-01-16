d10f6fff4a432feb440e62141a3ffae25f4ca9d1
Fithamlak <justiceofgod01@gmail.com>
2023-01-16 20:55:14 +0300

    Create a collection that keeps a list of books

---
+++ index.js
@@ -0,0 +1,16 @@
+// Create a collection that keeps a list of books
+
+const listOfBooks = [
+  {
+    title: 'atomic habit',
+    author: 'James Clear',
+  },
+  {
+    title: 'Rich dad poor dad',
+    author: 'Robert kiosaki',
+  },
+  {
+    title: 'Think and grow rich',
+    author: 'napleon hill',
+  },
+];
