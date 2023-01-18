56776adacad0ce6f163b8cf68d1795be55c202f5
Fithamlak <justiceofgod01@gmail.com>
2023-01-18 16:38:54 +0300

    change index.js to put books using array in localstorage and remove from it based on reviwer suggestion

--- index.js
+++ index.js
@@ -3,81 +3,85 @@ const addButton = document.querySelector('#add');
 const titleInput = document.querySelector('#title');
 const autherInput = document.querySelector('#author');
 
+// put variables and most used functions first to acces them
+let booksData = [];
+let availableStorage;
+let count = 0;
+
+function storeData(booksData) {
+  if (availableStorage) {
+    const jsonData = JSON.stringify(booksData);
+    availableStorage.setItem('books', jsonData);
+  }
+}
+
+function retrieveData() {
+  const bookData = availableStorage.getItem('books');
+  const parseBookData = JSON.parse(bookData);
+  if (parseBookData) {
+    booksData = parseBookData;
+  }
+}
+
 // define class with object constructor
 class Book {
   constructor(title, author) {
     this.title = title;
     this.author = author;
+    this.id = Math.random();
   }
 
-  static count = 0;
-
-  static listOfBooks = [];
-
-  static addBook(title, author) {
-    const newBook = new Book(title, author);
-    const book = document.createElement('article');
-    if (this.count % 2 === 0) {
-      book.style.backgroundColor = '#e5e5e5c4';
-    }
-    this.count += 1;
-    book.classList.add('article_content');
-    const titleByAuthor = document.createElement('p');
-    titleByAuthor.innerText = `"${newBook.title}" by ${newBook.author}`;
-    book.append(titleByAuthor);
-    const button = document.createElement('button');
-    button.innerText = 'Remove';
-    button.classList.add('remove');
-    button.addEventListener('click', () => {
-      book.style.display = 'none';
-    });
-    book.append(button);
-    dynamicCreation.append(book);
+  static addBook(book) {
+    const newBook = new Book(book.title, book.author);
+    booksData.push(newBook);
+    storeData(booksData);
+    window.location.reload();
   }
 
-  static removeBook(index) {
-    Book.listOfBooks.splice(
-      Book.listOfBooks.findIndex((e) => e.title === Book.listOfBooks[index].title
-      && e.author === Book.listOfBooks[index].author),
+  static removeBook(book) {
+    booksData.splice(
+      booksData.findIndex((e) => e.title === book.title
+      && e.author === book.author),
     );
-    this.count -= 1;
+    storeData(booksData);
   }
 }
 
-// creat objects
-const b1 = new Book('atomic habit', 'James Clear');
-const b2 = new Book('Rich dad poor dad', 'Robert kiosaki');
-Book.listOfBooks.push(b1);
-Book.listOfBooks.push(b2);
-
 // Display all books saved in the collection in the top part of the page.
-function bookLoders() {
-  for (let k = 0; k < Book.listOfBooks.length; k += 1) {
-    const book = document.createElement('article');
-    book.classList.add('article_content');
-    if (k % 2 === 0) {
-      book.style.backgroundColor = '#e5e5e5c4';
-    } else {
-      book.style.backgroundColor = 'white';
-    }
-    const title = document.createElement('p');
-    title.innerText = `"${Book.listOfBooks[k].title}" by ${Book.listOfBooks[k].author}`;
-    book.append(title);
-    const button = document.createElement('button');
-    button.classList.add('remove');
-    button.innerText = 'Remove';
-    button.addEventListener('click', () => {
-      Book.removeBook(k);
-      book.style.display = 'none';
-    });
-    book.append(button);
-    dynamicCreation.append(book);
-    dynamicCreation.classList.add('dynamic');
+function bookLoders(bookToBeLoad) {
+  const book = document.createElement('article');
+  book.classList.add('article_content');
+  if (count % 2 === 0) {
+    book.style.backgroundColor = '#e5e5e5c4';
+  } else {
+    book.style.backgroundColor = 'white';
   }
+  count += 1;
+  const title = document.createElement('p');
+  title.innerText = `"${bookToBeLoad.title}" by ${bookToBeLoad.author}`;
+  book.append(title);
+  const button = document.createElement('button');
+  button.classList.add('remove');
+  button.innerText = 'Remove';
+  button.addEventListener('click', () => {
+    Book.removeBook(book);
+    book.style.display = 'none';
+  });
+  book.append(button);
+  dynamicCreation.append(book);
+  dynamicCreation.classList.add('dynamic');
+}
+
+function displayToPage() {
+  booksData.forEach((book) => {
+    bookLoders(book);
+  });
 }
 
 addButton.addEventListener('click', () => {
-  Book.addBook(titleInput.value, autherInput.value);
+  const newBook = new Book(titleInput.value, autherInput.value);
+  Book.addBook(newBook);
+  displayToPage(newBook);
 });
 //  data is preserved in the browser's memory by using localStorage.
 // check local storage available
@@ -111,8 +115,6 @@ function storageAvailable(type) {
   }
 }
 
-let availableStorage;
-
 if (storageAvailable('localStorage')) {
   // Yippee! We can use localStorage awesomeness
   availableStorage = window.localStorage;
@@ -120,38 +122,9 @@ if (storageAvailable('localStorage')) {
   // Too bad, no localStorage for us
   availableStorage = null;
 }
-// create a single object for the data
-const inputDataObject = {};
-// function to store data in a loclaStorage
-function storeData() {
-  inputDataObject.title = titleInput.value;
-  inputDataObject.author = autherInput.value;
-  const jsonData = JSON.stringify(inputDataObject);
-  availableStorage.setItem('InputData', jsonData);
-}
-
-// listen to change on input fields
-titleInput.addEventListener('change', () => {
-  storeData();
-});
-
-autherInput.addEventListener('change', () => {
-  storeData();
-});
-
-// retrive the data by stringify and
-// set the object to local on loads of page
-function retrieveData() {
-  const bookData = availableStorage.getItem('InputData');
-  const parseBookData = JSON.parse(bookData);
-  if (bookData?.length > 0) {
-    const { title, author } = parseBookData;
-    titleInput.value = title || '';
-    autherInput.value = author || '';
-  }
-}
+// create a single object for the dat
 
 window.onload = () => {
   retrieveData();
-  bookLoders();
+  displayToPage();
 };
